2375c2e33dd01586f2ec05bb214d58620eac27a3
TimurUsman <usmant667@gmail.com>
2024-11-29 21:27:18 +0300

    Lab3 completed

--- lab3_task/lab_3_4_task.cpp
+++ lab3_task/lab_3_4_task.cpp
@@ -1,44 +1,113 @@
 ﻿#include <iostream>
 #include <vector>
 #include <iomanip>
- 
+#include <string>
+#include <sstream>
+#include <fstream>
+
 using namespace std;
 
-void minMaxScaler(const vector<double>& arr, vector<double>& scaledArr) {
-    // If array is empty, then scaledArr is empty too
-    if (arr.empty()) {
-        scaledArr.clear();
-        return;
+class MinMaxScaler {
+public:
+    // O(n), where n - number of elements in inputArr
+    MinMaxScaler(vector<double>& inputArr) : arr(inputArr) {
+        normalize(arr); // O(n), where n - number of elements in arr
     }
 
-    double minVal = *min_element(arr.begin(), arr.end());
-    double maxVal = *max_element(arr.begin(), arr.end());
-
-    // if minVal == maxVal, fill scaledArr with 0.0
-    if (minVal == maxVal) {
-        scaledArr.assign(arr.size(), 0.0);
-        return;
+    const vector<double>& getScaledArray() const {
+        return arr;
     }
 
-    // Calculating each scaledValue and adding them in scaledArr
-    for (double num : arr) {
-        double scaledValue = (num - minVal) / (maxVal - minVal);
-        scaledArr.push_back(scaledValue);
+    void printScaledArray() const {
+        cout << setprecision(4) << fixed;
+        for (size_t i = 0; i < arr.size(); ++i) {
+            cout << scientific << arr[i];
+            if (i != arr.size() - 1) cout << " ";
+        }
+        cout << "]" << endl;
     }
+
+    void saveScaledArrayToFile(const string& filename) const {
+        ofstream outFile(filename);
+        if (!outFile.is_open()) {
+            cerr << "Unable to open file: " << filename << endl;
+            return;
+
+        }
+
+        for (size_t i = 0; i < arr.size(); ++i) {
+            outFile << fixed << setprecision(4) << arr[i];
+            if (i != arr.size() - 1) {
+                outFile << ",";
+            }
+        }
+        outFile.close();
+        cout << "Successfully saved data to file: " << filename << endl;
+    }
+
+private:
+    vector<double> arr;
+
+    void normalize(vector<double>& inputArr) {
+        // If array is empty, then scaledArray is empty too
+        if (inputArr.empty()) {
+            return; // O(1)
+        }
+
+        // Finding minVal and maxVal of the array
+        int minVal = *min_element(inputArr.begin(), inputArr.end()); // O(n)
+        int maxVal = *max_element(inputArr.begin(), inputArr.end()); // O(n)
+
+        // If min == max, then fill arr with 0.0
+        if (minVal == maxVal) {
+            arr.resize(inputArr.size(), 0.0); // Create a vector with the same size and fill with 0.0
+            return;
+        }
+
+        // Normalizing
+        arr.resize(inputArr.size()); // Resize arr to store scaled values
+        for (size_t i = 0; i < inputArr.size(); ++i) {
+            arr[i] = static_cast<double>(inputArr[i] - minVal) / (maxVal - minVal); // O(n)
+        }
+    }
+};
+
+vector<double> readDataFromFile(const string& filename) {
+    ifstream file(filename);
+    vector<double> data;
+    string line;
+
+    if (!file.is_open()) {
+        cerr << "Не удалось открыть файл: " << filename << endl;
+        return data;
+    }
+
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string item;
+
+        while (getline(ss, item, ',')) {
+            try {
+                data.push_back(stod(item)); // Convert to integer (double)
+            }
+            catch (const invalid_argument& e) {
+                cerr << "Error converting string: " << item << endl;
+            }
+        }
+    }
+
+    return data;
+
 }
 
 int main() {
-    vector<double> arr = { 1, 100, 1000001, 2, 900000, 1000000, 7};
-    vector<double> scaledArr = {};
+    string filename = "C:\\Study\\dataset_file3.txt";
 
-    minMaxScaler(arr, scaledArr);
+    vector<double> myArray = readDataFromFile(filename);
 
-    cout << setprecision(1) << "[";
-    for (size_t i = 0; i < scaledArr.size(); ++i) {
-        cout << scientific << scaledArr[i];
-        if (i != scaledArr.size() - 1) cout << " ";
-    }
-    cout << "]" << endl;
+    MinMaxScaler sc(myArray);
+    sc.printScaledArray();
+    sc.saveScaledArrayToFile("scaledDataset3.txt");
 
     return 0;
 }
